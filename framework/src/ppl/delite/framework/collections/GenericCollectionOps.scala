7e89f977301899e0c5ed842b2ffa0e67fbbdcc77
aleksandar <aleksandar@lampmac14.epfl.ch>
2011-10-12 10:44:50 +0200

    Some minor fixes after merging.

--- framework/src/ppl/delite/framework/collections/GenericCollectionOps.scala
+++ framework/src/ppl/delite/framework/collections/GenericCollectionOps.scala
@@ -6,7 +6,6 @@ import java.io.PrintWriter
 import ppl.delite.framework.{DeliteApplication, DSLType}
 import ppl.delite.framework.datastruct.scala.DeliteCollection
 import scala.virtualization.lms.common._
-import ppl.delite.framework.collections.datastruct.scala._
 import ppl.delite.framework.ops.DeliteOpsExp
 
 
@@ -17,7 +16,7 @@ trait GenericCollectionOps extends DSLType with Variables {
   trait CanBuild[-Coll, -S, +Target] {
     def alloc(source: Rep[Coll]): Rep[Target]
     def emptyAlloc(source: Rep[Coll]): Rep[Target]
-    def emitter(source: Rep[Coll]): Emitter
+    def emitterScala(source: Rep[Coll]): ScalaEmitter
   }
   
 }
@@ -27,21 +26,16 @@ trait GenericCollectionGen {
 }
 
 
-trait EmitterProvider {
-  def emitterScala: Emitter
-}
-
-
 /** Collection construction emitter. Given that the collection can be emitted for a given
  *  platform, uses the evidence to emit the code.
  */
-trait Emitter {
+trait ScalaEmitter {
   def emitBufferDefs(basename: String, elemtype: String)(implicit stream: PrintWriter)
   def emitInitSubActivation(basename: String, activname: String)(implicit stream: PrintWriter)
   def emitAddToBuffer(basename: String, activname: String, elemname: String)(implicit stream: PrintWriter)
+  def emitAddToDataStructure(prefixSym: String, basename: String, elemname: String)(implicit stream: PrintWriter)
   def emitPostCombine(basename: String, activname: String, lhsname: String)(implicit stream: PrintWriter)
   def emitPostProcInit(basename: String, activname: String)(implicit stream: PrintWriter)
   def emitPostProcess(basename: String, activname: String)(implicit stream: PrintWriter)
 }
-
 
