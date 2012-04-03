3700b9d1a683689c2dc05b22f343752f1a5e9669
Manohar Jonnalagedda <manojo10386@gmail.com>
2012-04-03 18:20:16 +0200

    cleanup

--- profiler/page_behaviour.js
+++ profiler/page_behaviour.js
@@ -1,12 +1,15 @@
 // Override the onload event
 $(document).ready(function(){
- $(".gen-sourcefile-header").each(function (index, domEle){
-   //  alert("whaaa");
+    toggleGenFiles();
+//    displaySymbols();
+});
+
+
+function toggleGenFiles(){
+  $(".gen-sourcefile-header").each(function (index, domEle){
+    //  alert("whaaa");
     $(domEle).click(function(){
       $(domEle).next().toggle();
     });    
- });
- 
- 
- // the page finished loading, do something ...
-});
+  });
+}
